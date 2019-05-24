17fe9a9d5af24c47b4c8f1d14c8ad5a642c7cd33
Irene Savostyanova <irsavostyanova@gmail.com>
2019-05-24 19:27:09 +0300

    add hibernate config

--- src/main/java/com/interlink/quiz/ApplicationConfig.java
+++ src/main/java/com/interlink/quiz/ApplicationConfig.java
@@ -7,9 +7,42 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.util.Properties;
 
 @Configuration
 public class ApplicationConfig {
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.interlink.quiz.object");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+
+        return sessionFactory;
+    }
+
+    @Bean
+    public PlatformTransactionManager hibernateTransactionManager() {
+        HibernateTransactionManager transactionManager
+                = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+        return transactionManager;
+    }
+
+    private Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty(
+                "hibernate.ddl-auto", "create-drop");
+        hibernateProperties.setProperty(
+                "hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+
+        return hibernateProperties;
+    }
 
     @Bean
     public HikariDataSource dataSource() {
---
+++ src/main/java/com/interlink/quiz/object/Topic.java
@@ -0,0 +1,29 @@
+package com.interlink.quiz.object;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "topics")
+public class Topic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
