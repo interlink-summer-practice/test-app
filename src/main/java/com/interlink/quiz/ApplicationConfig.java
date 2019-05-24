93279875fde534be650328be26ae218418e8b180
Irene Savostyanova <irsavostyanova@gmail.com>
2019-05-24 20:20:08 +0300

    application config back to life

--- src/main/java/com/interlink/quiz/ApplicationConfig.java
+++ src/main/java/com/interlink/quiz/ApplicationConfig.java
@@ -7,18 +7,49 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
 
-import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 public class ApplicationConfig {
 
     @Bean
-    public DataSource dataSource() {
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.interlink.quiz.object");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+
+        return sessionFactory;
+    }
+
+    @Bean
+    public PlatformTransactionManager hibernateTransactionManager() {
+        HibernateTransactionManager transactionManager
+                = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+        return transactionManager;
+    }
+
+    private Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty(
+                "hibernate.ddl-auto", "create-drop");
+        hibernateProperties.setProperty(
+                "hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+
+        return hibernateProperties;
+    }
+
+    @Bean
+    public HikariDataSource dataSource() {
         HikariConfig config = new HikariConfig();
-        config.setJdbcUrl("jdbc:postgresql://localhost:5432/quizes");
+        config.setJdbcUrl("jdbc:postgresql://localhost:5432/tests");
         config.setUsername("postgres");
-        config.setPassword("m1a1d1r1i1d1S4");
+        config.setPassword("123456");
         config.setDriverClassName("org.postgresql.Driver");
 
         return new HikariDataSource(config);
