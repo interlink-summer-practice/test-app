72708e9107b39dce050d9ff1276e6c8464b23a0b
IreneCat <irsavostyanova@gmail.com>
2019-06-04 16:04:35 +0300

    change return value for registration

--- src/main/java/com/interlink/quiz/controller/RegistrationController.java
+++ src/main/java/com/interlink/quiz/controller/RegistrationController.java
@@ -3,6 +3,7 @@ package com.interlink.quiz.controller;
 import com.interlink.quiz.object.dto.UserDto;
 import com.interlink.quiz.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,13 +23,13 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public ResponseEntity<String> register(@Valid @RequestBody UserDto userDto) {
+    public ResponseEntity register(@Valid @RequestBody UserDto userDto) {
         if (userService.getUserByEmail(userDto.getEmail()) == null) {
             userService.register(userDto);
 
-            return ResponseEntity.ok("User is valid");
+            return new ResponseEntity(HttpStatus.OK);
         }
 
-        return ResponseEntity.ok("This email already in use");
+        return new ResponseEntity(HttpStatus.BAD_REQUEST);
     }
 }
