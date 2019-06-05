b7588a99a094d64511583bd1a1da51047d6b8aef
Oleksandr Rolko <sasha.rolko17@gmail.com>
2019-06-05 11:20:04 +0300

    deleted migration

--- src/main/resources/db/migration/V20190605094229__add_subtopic_id_column_to_topics_table.sql
+++
@@ -1,7 +0,0 @@
-alter table topics
-    add subtopic_id int;
-
-alter table topics
-    add constraint topics_topics_id_fk
-        foreign key (subtopic_id) references topics
-            on update cascade on delete cascade;
