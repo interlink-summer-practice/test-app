505a312bcbb5bb5c0f78abe4057283bd61856937
YKyrylenko <yaroslavkirilenko1@gmail.com>
2019-06-10 10:30:10 +0300

    component is ready to style

---
+++ src/components/quiz-time-line/QuizTimeLine.js
@@ -0,0 +1,15 @@
+import React , { Component} from 'react';
+
+export default class QuizTimeLine extends Component {
+
+
+    render()
+     {
+     console.log(this.props.currentNumberOfQuestion);
+     console.log(this.props.numberOfQuestions);
+
+        return(
+        <div></div>
+        )
+    }
+}
