be5f052793fb712d2cc15c1a9d16ff66f5316c13
IreneCat <irsavostyanova@gmail.com>
2019-06-12 14:16:04 +0300

    del migration : add column quiz session id in group members table

--- src/main/resources/db/migration/V20190612140814__add_column_quiz_session_id_in_qroup_members_table.sql
+++
@@ -1,7 +0,0 @@
-alter table group_members
-    add quiz_session_id int;
-
-alter table group_members
-    add constraint group_members_quiz_session_id_fk
-        foreign key (quiz_session_id) references quiz_session
-            on update cascade on delete cascade;
